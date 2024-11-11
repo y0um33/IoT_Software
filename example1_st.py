9f24b3d8c6b82d01cc8b3c81c9e30ccde73ecef0
Yumee Chung <137648897+y0um33@users.noreply.github.com>
2024-11-11 07:33:31 -0600

    Update example1_st.py

--- example1_st.py
+++ example1_st.py
@@ -7,7 +7,7 @@ df = pd.read_csv("Policev1.csv")
 
 st.markdown(':rainbow[The data shown below belongs to incident report in the city of San Francisco, from the year 2018 to 2020, with details from each case sucas as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution]')
 
-mapa = pd.DataFram()
+mapa = pd.DataFrame()
 mapa['Date'] = df['Incident Date']
 mapa['Day'] = df['Incident Day of Week']
 mapa['Police District'] = df['Police District']
