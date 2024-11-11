1a7a92020c6d7550d3bfd265c104e1976c444dc8
Yumee Chung <137648897+y0um33@users.noreply.github.com>
2024-11-11 07:27:07 -0600

    Update example1_st.py

--- example1_st.py
+++ example1_st.py
@@ -5,4 +5,4 @@ st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 
 df = pd.read_csv("Policev1.csv")
 
-st.markdown('The data shown below belongs to incident report in the city of San Francisco, from the year 2018 to 2020, with details from each case sucas as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution')
+st.markdown(':rainbow[The data shown below belongs to incident report in the city of San Francisco, from the year 2018 to 2020, with details from each case sucas as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution]')
